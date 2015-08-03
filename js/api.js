5fc63fd87d29ede504c64dd7a60998943cfc31f2
hwagner1 <hbw6955@rit.edu>
2015-08-03 16:48:24 -0400

    Commented Api.js

--- js/api.js
+++ js/api.js
@@ -1,19 +1,41 @@
+//Main object that holds functions that utilize the Adobe DPS API and allow elements...
+//to interact with the back end of the app.
+var api = {
 
-$( document ).ready(function() {
-	var $sliderOne = $( '#slideshow-container-one' ),
-		$sliderTwo = $( '#slideshow-container-two' ),
-		$buttonNav = $( '.button.nav' );
 
-	adobeDPS.Gesture.disableNavigation([$sliderOne[0], $sliderTwo[0]]);
 
-	$buttonNav.attr( "onclick", "toggleNavUi();" );
-	//$buttonNav.html( "SHOW NAV" );
-});
+  /**
+   * setUp
+   *   Initial function that uses jquery to select elements of the app, as documented...
+   *   in the Adobe DPS API documentation.
+   */
+  setUp: function(){
+    $( document ).ready(function() {
+      var $sliderOne = $( '#slideshow-container-one' );
+      var $sliderTwo = $( '#slideshow-container-two' );
 
-$toggle = 0;
+      //button that I have added to the DOM to navigate back with
+      var $buttonNav = $( '.button.nav' );
 
-function toggleNavUi(){
-	adobeDPS.Gesture.toggleNavigationUI();
-  window.location = "navto://relative/last";
-	//$toggle == 0 ? $( ".button.nav" ).html( "NAV!!!" ) && $toggle++ : $( ".button.nav" ).html( "SHOW NAV" ) && $toggle--;
+      //sets the default app navigation as hidden
+      adobeDPS.Gesture.disableNavigation([$sliderOne[0], $sliderTwo[0]]);
+
+      //binds event listener to my button element
+      $buttonNav.attr( "onclick", "api.toggleNavUi();" );
+    });
+  }
+
+
+
+  /**
+   * toggleNavUi
+   *   Function that should toggle the visibility of the apps built in navigation...
+   *   as documented in the Adobe DPS API documentation. (Added old DPS relative...
+   *   link to see if possible to just go back in the app instead of clicking nav).
+   *
+   */
+  toggleNavUi: function(){
+    adobeDPS.Gesture.toggleNavigationUI();
+    window.location = "navto://relative/last";
+  }
 }
